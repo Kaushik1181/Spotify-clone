79f2b950d266ce3503dd818089442ac10e96b198
Kaushik <47687763+Kaushik1181@users.noreply.github.com>
2020-12-02 22:18:37 +0530

    Initial commit

---
+++ src/spotify.js
@@ -0,0 +1,29 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "https://gsg25.csb.app/";
+
+const clientId = "df3072b32a4d4887b65ac8c9abd2ff25";
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state"
+];
+
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      var parts = item.split("=");
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+
+      return initial;
+    }, {});
+};
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
